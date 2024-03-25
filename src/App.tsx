b092682651df1e1a831c2cca73ff1dd95a5bea52
Sergey <kolesnik.s@future-group.ru>
2024-03-25 21:36:22 +0300

    initial 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React, { useEffect } from "react";
 import "./App.css";
 
 const tg = window.Telegram.WebApp;
+const url = "https://mellow-florentine-74b2ce.netlify.app";
 
 function App() {
   useEffect(() => {
+    tg.expand();
     tg.ready();
-  });
+  }, []);
 
   const onClose = () => {
     tg.close();
